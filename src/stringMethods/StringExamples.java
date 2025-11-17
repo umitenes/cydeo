fae39b7ae80484b4acfeb28bd24ab22f969f6d20
umitakdag <umitakdag@gmail.com>
2025-11-17 08:32:15 +0200

    added

---
+++ src/stringMethods/StringExamples.java
@@ -0,0 +1,15 @@
+package stringMethods;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        String str1="Hello";
+        String str2=new String("Hello");
+
+        System.out.println(str1.equals(str2)); // true
+
+        System.out.println(str1==str2); //false
+
+    }
+}
