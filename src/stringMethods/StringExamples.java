cf2de28be2085cb3b830cd2739d97d997e428fbe
umitakdag <umitakdag@gmail.com>
2025-11-17 08:41:20 +0200

    added new code

--- src/stringMethods/StringExamples.java
+++ src/stringMethods/StringExamples.java
@@ -2,14 +2,7 @@ package stringMethods;
 
 public class StringExamples {
     public static void main(String[] args) {
-        System.out.println("Hello");
 
-        String str1="Hello";
-        String str2=new String("Hello");
-
-        System.out.println(str1.equals(str2)); // true
-
-        System.out.println(str1==str2); //false
-
+        System.out.println("Hello ICHC");
     }
 }
